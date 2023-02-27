7fddb8da72ee0eb6babde3737704f5a29d1afb85
fakhriyya rahimova <rahimovafakhriyya@gmail.com>
2023-02-27 16:28:06 +0100

    adding equipments and bookings controller

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,25 @@
 class BookingsController < ApplicationController
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+    @equipment = Equipment.find(params[:equipment_id])
+    @booking = Booking.new(booking_params)
+    @booking.equipment = @booking
+    @booking.save
+
+    redirect_to equipment_path(@booking)
+  end
+
+  def destroy
+    @booking.destroy
+    redirect_to equipments_path, status: :see_other
+  end
+
+  private
+
+  def booking_params
+    params.require(:booking).permit(:equipment_id)
+  end
 end
