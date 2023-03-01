2f0399e6656c7ff4fafc4f250b30f7d20e8fe4db
Angie Xu <anyixu98@gmail.com>
2023-03-01 15:50:12 +0100

    assets

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -24,6 +24,7 @@ Rails.application.configure do
   # Apache or NGINX already handles this.
   config.public_file_server.enabled = ENV["RAILS_SERVE_STATIC_FILES"].present?
 
+
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
@@ -47,6 +48,8 @@ Rails.application.configure do
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
+  config.serve_static_assets = true
+
 
   # Include generic and useful information about system operation, but avoid logging too much
   # information to avoid inadvertent exposure of personally identifiable information (PII).
