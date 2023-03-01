7d619d1e4408fc9ace92956b7658793181a4b50e
Tahli Kipps <tahli.kipps@gmail.com>
2023-03-01 12:27:30 +0100

    booking index page

--- app/javascript/controllers/flatpickr_controller.js
+++ app/javascript/controllers/flatpickr_controller.js
@@ -1,7 +1,15 @@
-import { Controller } from "@hotwired/stimulus"
+import { Controller } from '@hotwired/stimulus';
 
-// Connects to data-controller="flatpickr"
+import flatpickr from "flatpickr";
+import rangePlugin from "flatpickr/dist/plugins/rangePlugin";
+
 export default class extends Controller {
   connect() {
+    flatpickr(this.element, {
+      altInput: true,
+        plugins: [new rangePlugin({
+          input: "#booking_end_date"
+        })]
+    });
   }
 }
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_28_084846) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_01_111206) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -50,6 +50,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_28_084846) do
     t.integer "total_price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "status", default: 0
     t.index ["equipment_id"], name: "index_bookings_on_equipment_id"
     t.index ["user_id"], name: "index_bookings_on_user_id"
   end
