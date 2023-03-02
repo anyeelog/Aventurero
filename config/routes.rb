c5931257a14f8133d89694fb9e1fb61eef7de611
FatimaPashazade <fatima.pashazadeh20@gmail.com>
2023-03-02 17:24:33 +0100

    Profile page

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  resources :users, :only => [:show]
 
   resources :equipments do
     resources :bookings, only: %i[new create]
